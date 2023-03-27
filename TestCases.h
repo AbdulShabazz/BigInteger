6f8f34b9ff6e468a5f9d52a3a98aee248c2413d3
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-03-27 09:20:33 -0400

    Update TestCases.h

--- TestCases.h
+++ TestCases.h
@@ -2,62 +2,81 @@
 
 #include "__x86i64Int.h"
 #include <cassert>
-
+#include <exception>
+#define _DEBUG
 #ifdef _DEBUG
 	class x86i64IntTestCases
 	{
 	public:
 		x86i64IntTestCases()
 		{
-			std::__x86i64Int _a_{}; // OK
-			std::__x86i64Int a{ 12n }; // OK
-			//std::__x86i64Int aa{ 12 }; // Error
-			std::__x86i64Int aaa{ "0" };
-			std::__x86i64Int aaaa{ "12" }; // OK
-			std::__x86i64Int b{ 0n };
-			std::__x86i64Int c = std::__x86i64Int{ 12n }; // OK
-			//std::__x86i64Int cc = std::__x86i64Int{ 12 }; // Error
-			std::__x86i64Int ccc = std::__x86i64Int{ "12" }; // OK
-			std::__x86i64Int d = std::__x86i64Int{ 12n };
-			std::__x86i64Int e = 12n;
-			std::__x86i64Int f(0);
-			// a = b = 12n
-			assert(a == 12n);
-			assert(b == 12n);
-			assert(a + b == std::__x86i64Int{ "24" });
-			assert(c* d == std::__x86i64Int{ "144" });
-			std::pair<std::__x86i64Int, std::__x86i64Int> aa = a / b;
-			assert(aa.first == std::__x86i64Int{ "1" });
-			assert(aa.second == std::__x86i64Int{ "0" });
-			assert(a - b == std::__x86i64Int{ "0" });
-			assert(a++ == std::__x86i64Int{ "12" });
-			assert(a == std::__x86i64Int{ "13" });
-			assert(a-- == std::__x86i64Int{ "13" });
-			assert(a == std::__x86i64Int{ "12" });
-			++a;
-			assert(--a == std::__x86i64Int{ "12" });
-			assert(a% b == std::__x86i64Int{ "0" });
-			a %= b;
-			assert(a == std::__x86i64Int{ "0" });
+			std::__x86i64Int a{ "191" };
+			std::cout << "a: " << (a) << std::endl;
+			std::__x86i64Int b{ "119" };
+			std::cout << "b: " << b << std::endl;
+			std::__x86i64Int c = a + b;
+			std::cout << "c = a + b: " << c << std::endl;
+			std::__x86i64Int d = a * b;
+			std::cout << "d = a * b: " << d << std::endl;
+			std::__x86i64Int e = a / b;
+			std::cout << "e = a / b, \ne.quotient(): " << e.quotient() << ", \ne.remainder() : " << e.remainder() << std::endl;
+			std::__x86i64Int _a_{};
+			a = 12n;
+			std::cout << "a = 12n: " << b << std::endl;
+			try 
+			{
+				// std::__x86i64Int aa( 12 ); // Compile Error
+			}
+			catch (const std::exception& e)
+			{
+				std::cout << "std::__x86i64Int aa( 12 ) // Compiler Error: " << e.what() << std::endl;
+			}
+			try
+			{
+				// std::__x86i64Int aa(0); // Runtime Error
+			}
+			catch (const std::exception& e)
+			{
+				std::cout << "std::__x86i64Int aa( 0 ) // Runtime Error: " << e.what() << std::endl;
+			}
+			std::__x86i64Int aaaa{ "12" };
+			b = 0n;
+			c = std::__x86i64Int{ 12n };
+			std::__x86i64Int ccc = std::__x86i64Int{ "12" };
+			d = std::__x86i64Int{ 12n };
+			std::__x86i64Int f{ 0n };
+			a = b = 12n;
+			std::cout << "a = b = 12n, a: " << a << ", b: " << b << std::endl;
+			std::cout << "a + b // 24 : " << a + b << std::endl;
+			std::cout << "c * d // 144 : " << c * d << std::endl;
+			std::cout << "a - b // 0 : " << a - b << std::endl;
+			std::cout << "a++ // 12 : " << a++ << std::endl;
+			std::cout << "a // 13 : " << a << std::endl;
+			std::cout << "a-- // 13 : " << a-- << std::endl;
+			std::cout << "a // 12: " << a << std::endl;
+			std::cout << "++a // 13 : " << ++a << std::endl;
+			std::cout << "a // 13 : " << a << std::endl;
+			std::cout << "--a // 12 : " << --a << std::endl;
+			std::cout << "a // 12 : " << a << std::endl;
+			std::cout << "a %= b // 0 : " << (a %= b) << std::endl;
 			a = 12n;
 			a += b;
-			assert(a == std::__x86i64Int{ "24" });
+			std::cout << "a = 12n; a += b; // 24n : " << a << std::endl;
 			a -= b;
-			assert(a == std::__x86i64Int{ "12" });
+			std::cout << "a -= b // 12n : " << a << std::endl;
 			a *= b;
-			assert(a == std::__x86i64Int{ "144" });
+			std::cout << "a *= b // 144: " << a << std::endl;
+			std::cout << "a == std::__x86i64Int{ \"144\" }) // True: " << (a == std::__x86i64Int{ "144" }) << std::endl;
 			a /= b;
-			assert(a.first == 1n);
-			assert(a.second == 01n);
-			assert(a < b == false);
-			assert(a > b == false);
-			assert(a <= b == true);
-			assert(a >= b == true);
-			assert(a == b && b == 12n);
-			assert(a != b == false);
-			assert(a& b == 12n);
-			a &= b;
-			assert(a == b == 12n);
+			std::cout << "(a /= b) // a.quotient() == 1n: " << a.quotient() << std::endl;
+			std::cout << "(a /= b) // a.remainder() == 0n: " << a.remainder() << std::endl;
+			std::cout << "a < b == false : " << (a < b) << std::endl;
+			std::cout << "a > b == false : " << (a > b) << std::endl;
+			std::cout << "a <= b == true : " << (a <= b) << std::endl;
+			std::cout << "a >= b == true : " << (a >= b) << std::endl;
+			std::cout << "a == b && b == 12n : " << (a == b && b == 12n) << std::endl;
+			std::cout << "a != b == false : " << (a != b) << std::endl;
+			std::cout << "a & b == 12 // 12 : " << (a & b) << std::endl;
 		};
 	};
 #endif
